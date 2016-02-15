7fc12b7388ed4ae5b7e2efc647566af9c607e568
Lucas Morais <lucasm310@gmail.com>
2016-02-15 12:00:54 -0200

    ADD TROCO

--- angular/pratica/app.js
+++ angular/pratica/app.js
@@ -3,79 +3,84 @@ angular.module("cardapio").controller("cardapioCtrl", function ($scope) {
 	$scope.app = "Cardapio";
 	//Alterar os produtos mas manter a formação
 	$scope.produtos = [
-		{nome: "Yakisoba Carne", valor: "18", categoria: "Yakisoba"}, 
-		{nome: "Yakisoba Frango", valor: "18", categoria: "Yakisoba"}, 
-		{nome: "Yakisoba Salmão", valor: "20", categoria: "Yakisoba"}, 
-		{nome: "Yakisoba Camarão", valor: "20", categoria: "Yakisoba"}, 
-		{nome: "Yakisoba Carne de Porco", valor: "18", categoria: "Yakisoba"}, 
-		{nome: "Yakisoba Carne & Frango", valor: "20", categoria: "Yakisoba"}, 
-		{nome: "Yakisoba Camarão & Salmão", valor: "22", categoria: "Yakisoba"}, 
-		{nome: "Yakisoba Carne , Frango e Carne de Porco", valor: "23", categoria: "Yakisoba"},
+		{nome: "Yakisoba Carne", valor: "20", categoria: "Yakisoba"}, 
+		{nome: "Yakisoba Frango", valor: "20", categoria: "Yakisoba"}, 
+		{nome: "Yakisoba Salmão", valor: "23", categoria: "Yakisoba"}, 
+		{nome: "Yakisoba Camarão", valor: "23", categoria: "Yakisoba"}, 
+		{nome: "Yakisoba Carne de Porco", valor: "20", categoria: "Yakisoba"}, 
+		{nome: "Yakisoba Carne & Frango", valor: "22", categoria: "Yakisoba"}, 
+		{nome: "Yakisoba Camarão & Salmão", valor: "26", categoria: "Yakisoba"}, 
+		{nome: "Yakisoba Carne , Frango e Carne de Porco", valor: "25", categoria: "Yakisoba"},
 		// Sushi
-		{nome: "Sushi de Omelete", valor: "14", categoria: "Sushi"}, 
-		{nome: "Sushi de Legume", valor: "13", categoria: "Sushi"}, 
-		{nome: "Sushi de Atum", valor: "14", categoria: "Sushi"}, 
-		{nome: "Sushi de Kani", valor: "15", categoria: "Sushi"}, 
-		{nome: "Sushi de Salmão", valor: "17", categoria: "Sushi"}, 
-		{nome: "Sushi de Camarão", valor: "17", categoria: "Sushi"}, 
+		{nome: "Sushi de Omelete", valor: "15", categoria: "Sushi"}, 
+		{nome: "Sushi de Legume", valor: "14", categoria: "Sushi"}, 
+		{nome: "Sushi de Atum", valor: "15", categoria: "Sushi"}, 
+		{nome: "Sushi de Kani", valor: "16", categoria: "Sushi"}, 
+		{nome: "Sushi de Salmão", valor: "18", categoria: "Sushi"}, 
+		{nome: "Sushi de Camarão", valor: "18", categoria: "Sushi"}, 
 		// Uramaki
-		{nome: "Uramaki Legumes", valor: "13", categoria: "Urumaki"}, 
-		{nome: "Uramaki Atum", valor: "13", categoria: "Urumaki"}, 
-		{nome: "Uramaki de Kani", valor: "16", categoria: "Urumaki"}, 
-		{nome: "Uramaki de Salmão Coberto", valor: "24", categoria: "Urumaki"}, 
-		{nome: "Uramaki de Salmão", valor: "17", categoria: "Urumaki"}, 
-		{nome: "Uramaki de Camarão", valor: "17", categoria: "Urumaki"}, 
+		{nome: "Uramaki Legumes", valor: "15", categoria: "Urumaki"}, 
+		{nome: "Uramaki Atum", valor: "16", categoria: "Urumaki"}, 
+		{nome: "Uramaki de Kani", valor: "17", categoria: "Urumaki"}, 
+		{nome: "Uramaki de Salmão Coberto", valor: "27", categoria: "Urumaki"}, 
+		{nome: "Uramaki de Salmão", valor: "19", categoria: "Urumaki"}, 
+		{nome: "Uramaki de Camarão", valor: "19", categoria: "Urumaki"},
+                {nome: "Uramaki de Omelete", valor: "16", categoria: "Urumaki"}, 
 		//Temaki
-		{nome: "Temaki Atum", valor: "16", categoria: "Temaki"}, 
-		{nome: "Temaki Kani", valor: "16", categoria: "Temaki"}, 
-		{nome: "Temaki Salmão", valor: "18", categoria: "Temaki"}, 
-		{nome: "Temaki Camarão", valor: "18", categoria: "Temaki"}, 
-		{nome: "Temaki Salmão & Camarão", valor: "20", categoria: "Temaki"}, 
-		{nome: "Temaki Frito – Qualquer sabor", valor: "20", categoria: "Temaki"}, 
-		//Hot Roll
-		{nome: "Hot Roll de Legumes", valor: "14", categoria: "Hot Roll"}, 
-		{nome: "Hot Roll Omelete", valor: "14", categoria: "Hot Roll"}, 
-		{nome: "Hot Roll Kani", valor: "16", categoria: "Hot Roll"}, 
-		{nome: "Hot Roll Atum", valor: "16", categoria: "Hot Roll"}, 
-		{nome: "Hot Roll Estrogonofe Carne", valor: "16", categoria: "Hot Roll"}, 
-		{nome: "Hot Roll Camarão", valor: "17", categoria: "Hot Roll"}, 
-		{nome: "Hot Roll Salmão", valor: "17", categoria: "Hot Roll"}, 
+		{nome: "Temaki Atum", valor: "17", categoria: "Temaki"}, 
+		{nome: "Temaki Kani", valor: "18", categoria: "Temaki"}, 
+		{nome: "Temaki Salmão", valor: "20", categoria: "Temaki"}, 
+		{nome: "Temaki Camarão", valor: "20", categoria: "Temaki"}, 
+		{nome: "Temaki Salmão & Camarão", valor: "22", categoria: "Temaki"}, 
+		{nome: "Temaki Frito Atum", valor: "23", categoria: "Temaki Hot"},
+                {nome: "Temaki Frito Kani", valor: "23", categoria: "Temaki Hot"},
+                {nome: "Temaki Frito Salmão", valor: "23", categoria: "Temaki Hot"},
+                {nome: "Temaki Frito Camarão", valor: "23", categoria: "Temaki Hot"},
+                {nome: "Temaki Frito Salmão & Camarão", valor: "23", categoria: "Temaki Hot"},
+                //Hot Roll
+		{nome: "Hot Roll de Legumes", valor: "15", categoria: "Hot Roll"}, 
+		{nome: "Hot Roll Omelete", valor: "16", categoria: "Hot Roll"}, 
+		{nome: "Hot Roll Kani", valor: "17", categoria: "Hot Roll"}, 
+		{nome: "Hot Roll Camarão", valor: "20", categoria: "Hot Roll"}, 
+		{nome: "Hot Roll Salmão", valor: "20", categoria: "Hot Roll"}, 
 		//Niguiri
-		{nome: "Niguiri de Frango", valor: "14", categoria: "Niguiri"}, 
-		{nome: "Niguiri Salmão", valor: "20", categoria: "Niguiri"}, 
-		{nome: "Niguiri de Kani", valor: "16", categoria: "Niguiri"}, 
+		{nome: "Niguiri de Frango", valor: "16", categoria: "Niguiri"}, 
+		{nome: "Niguiri Salmão", valor: "22", categoria: "Niguiri"}, 
+		{nome: "Niguiri de Kani", valor: "17", categoria: "Niguiri"}, 
 		//Sashimi
-		{nome: "Sashimi com Cream Cheese", valor: "25", categoria: "Sashimi"}, 
-		{nome: "Sashimi", valor: "22", categoria: "Sashimi"}, 
+		{nome: "Sashimi com Cream Cheese", valor: "28", categoria: "Sashimi"}, 
+		{nome: "Sashimi", valor: "24", categoria: "Sashimi"}, 
 		//Rolinho Primavera
 		{nome: "Rolinho Primavera de Salmão", valor: "6", categoria: "Rolinho Primavera"}, 
 		{nome: "Rolinho Primavera de Camarão", valor: "6", categoria: "Rolinho Primavera"}, 
-		//"Frango Xadrez
-		{nome: "Frango Xadrez", valor: "20", categoria: "Frango Xadrez"}, 
+		//Arroz Shop Suey
+                {nome: "Arroz Shop Suey", valor: "22", categoria: "Arroz Shop Suey"},
+                //"Frango Xadrez
+		{nome: "Frango Xadrez", valor: "22", categoria: "Frango Xadrez"}, 
 		//Katsu Don
-		{nome: "Katsu Don", valor: "18", categoria: "Katsu Don"}, 
-		//KIT
-		{nome: "KIT A", valor: "80", categoria: "KIT"}, 
-		{nome: "KIT B", valor: "140", categoria: "KIT"}, 
-		{nome: "KIT C", valor: "230", categoria: "KIT"}, 
-		{nome: "KIT OURO", valor: "200", categoria: "KIT"}, 
-		{nome: "KIT PRATA", valor: "110", categoria: "KIT"}, 
-		{nome: "KIT BRONZE", valor: "70", categoria: "KIT"}, 
+		{nome: "Katsu Don", valor: "20", categoria: "Katsu Don"}, 
 		//Barca
-		{nome: "Barca 1", valor: "80", categoria: "Barca"}, 
-		{nome: "Barca 2", valor: "85", categoria: "Barca"}, 
+		{nome: "Barca 1", valor: "88", categoria: "Barca"}, 
+		{nome: "Barca 2", valor: "94", categoria: "Barca"}, 
 		{nome: "Barca 3", valor: "105", categoria: "Barca"}, 
-		{nome: "Barca 4", valor: "115", categoria: "Barca"}, 
-		{nome: "Barca 5", valor: "150", categoria: "Barca"}, 
-		{nome: "Barca 6", valor: "120", categoria: "Barca"}, 
-		{nome: "Barca 7", valor: "105", categoria: "Barca"}, 
-		{nome: "Barca 8", valor: "75", categoria: "Barca"}, 
-		//Bandeija
-		{nome: "Bandeija SushiJoe", valor: "40", categoria: "Bandeija"}
+		{nome: "Barca 4", valor: "125", categoria: "Barca"}, 
+		{nome: "Barca 5", valor: "165", categoria: "Barca"}, 
+		{nome: "Barca 6", valor: "132", categoria: "Barca"}, 
+		{nome: "Barca 7", valor: "125", categoria: "Barca"}, 
+		{nome: "Barca 8", valor: "83", categoria: "Barca"}, 
+		//Porcoes 15 unidades
+		{nome: "15 - Hot Roll Legumes", valor: "30", categoria: "Porções de 15 Unidades"},
+		{nome: "15 - Sushi e Joy", valor: "45", categoria: "Porções de 15 Unidades"},
+                {nome: "15 - Super Joy", valor: "56", categoria: "Porções de 15 Unidades"},
+		//Bento
+                {nome: "Teishoku", valor: "32", categoria: "Bento"},
+                {nome: "Combinado Pratico", valor: "35", categoria: "Bento"},
+                {nome: "Combinado Hot Master", valor: "55", categoria: "Bento"},
+                {nome: "Combinado Executivo", valor: "55", categoria: "Bento"},
 		];
 	/*Alterar abaixo o valor sendo que tem quer ser no padrão (3.00)
 	Alterar abaixo ativo com sim ou não (para promoção)*/
-	$scope.entrega = {valor: 3.40, ativo: "sim"};
+	$scope.entrega = {valor: 3.00, ativo: "nao"};
 
 /*-------------------------------------------------------------------------------------------------------------*/
 
@@ -87,6 +92,7 @@ angular.module("cardapio").controller("cardapioCtrl", function ($scope) {
 	{forma: "Dinheiro"},
 	{forma: "Cartão"},
 	];
+	$scope.troco = '';
 	$scope.adicionarProduto = function (pedido) {
 				$scope.pedidos.push(angular.copy(pedido));
 				delete $scope.pedido;
